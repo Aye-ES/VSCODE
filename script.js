bc9c8f3e4b534ba2f183f66f52f12d2742dddd7e
Aye-ES <ayelenestevezs@gmail.com>
2025-06-08 16:51:51 -0400

    agregando mas cambios

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
- let num_a =20
- let num_b = 10
- let resultado = num_a + num_b
- console.log(resultado)
+let num_a =20
+let num_b = 10
+let resultado = num_a + num_b
+console.log(resultado)
 
 // console.log("que onda")
